941694016895c9c997a84295d68011fc188d2ff7
Y_VRN <lr.ecracimo@mmdc.mcl.edu.ph>
2025-10-09 23:46:18 +0800

    Rewritten mode_selection.js
    Now checks if the page the mode buttons go to exists, and remembers of their existence. Also, links to non-existent pages are added so I have no need to push commits for all currently planned edit/view page I make.

--- scripts/mode_selection.js
+++ scripts/mode_selection.js
@@ -1,36 +1,78 @@
-/** @type {Map<string, string>} */
-const pages = new Map();
-pages.set("lsb.button-rack.dashboard", "experiment.html");
-pages.set("lsb.button-rack.timeline", "/pages/timeline.html");
+let fallbackPage = "/pages/fallback.html";
 
-let mainViewPane = document.getElementById("mv.view");
-let currentView = "pages/timeline.html";
+// NOTE: Just put currently non-existent links so you don't have to push
+// commits here for each page (maybe not for dashboard, still needs a
+// design).
+let idToLinkTargets = {}; {
+    idToLinkTargets[".fallback"] = "/pages/fallback.html";
+    idToLinkTargets["lsb.button-rack.dashboard"] = "/experiment.html";
+    idToLinkTargets["lsb.button-rack.timeline"] = "/pages/timeline.html";
+    idToLinkTargets["lsb.button-rack.calendar"] = "/pages/calendar.html";
+    idToLinkTargets["lsb.button-rack.alarms"] = "/pages/alarms.html";
+    idToLinkTargets["lsb.button-rack.kanban"] = "/pages/kanban.html";
+}
 
-function mv_page_navTo(htmlFile, force) {
-    // TODO: find some better alternative to matching file paths.
-    if (!force && currentView == htmlFile) {
+let visitedPages = {};
+
+let editingView = document.getElementById("mv.view");
+let activeId = "";
+
+for (const button of document.getElementById("lsb.button-rack").children) {
+    /** @type {string} */
+    let buttonId = button.getAttribute("id") || "";
+
+    button.addEventListener("click", () => {
+        trySwitchViewTo(buttonId || "")
+    });
+}
+ 
+
+
+/** @param {string} id */
+async function trySwitchViewTo(id) {
+    if (id == activeId) {
+        return;
+    } else if (
+        idToLinkTargets[id] == undefined
+    ) {
         return;
     } else {
-        mainViewPane.setAttribute("src", htmlFile);
-        currentView = htmlFile;
+        let link = idToLinkTargets[id];
+        
+        if (visitedPages[link] == true) {
+            _switchViewTo(id);
+            activeId = id;
+            return;
+        } else if (visitedPages[link] == false) {
+            return;
+        } else {
+            visitedPages[link] = false;
+        }
+
+        await fetch(idToLinkTargets[id])
+
+        .then( (result) => {
+            if (result.ok) {
+                visitedPages[link] = true;
+                _switchViewTo(id);
+            }
+
+            console.log(visitedPages);
+
+            activeId = id;
+        } );
     }
 }
 
-for (const lsbButton of document.getElementById("lsb.button-rack").children) {
-    /** @type {string} */
-    let htmlId = lsbButton.getAttribute("id") || "";
-    let htmlTargetSource = "";
-    
-    if (pages.has(htmlId)) {
-        htmlTargetSource = pages.get(htmlId);
-    }
 
-    console.log("Id:", htmlId, "| Source:", htmlTargetSource);
 
-    lsbButton.onclick = function() {
-        console.log("GOTO:", htmlTargetSource);
-        mv_page_navTo(htmlTargetSource);
-    }
+
+/* No input validation. Just to force a page switch, especially when
+loading the page for the first time in a new session */
+function _switchViewTo(id) {
+    editingView.textContent = null;
+    editingView.setAttribute("src", idToLinkTargets[id] || fallbackPage);
 }
 
-mv_page_navTo(currentView, true);
+
+trySwitchViewTo("lsb.button-rack.timeline")
