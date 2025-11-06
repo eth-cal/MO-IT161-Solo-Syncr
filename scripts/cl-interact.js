8f76173cac001dd71a1e1dba47cafe1d5d65cd75
Y_VRN <lr.ecracimo@mmdc.mcl.edu.ph>
2025-11-06 08:13:58 +0800

    Improved calendar code in new cl-interact.js
    Calendar rendering improved to visualise which days are outside the month. They are now aligned by the week days, starting with Sunday and ending with Saturday. Time is also now updated. New code should be usable now for monthly filtering. Currently missing the week day header, dialogs for adding tasks and navigating months, and the filtering options themselves. Might remove visibility options to merge with filtering options in the HTML file.

---
+++ scripts/cl-interact.js
@@ -0,0 +1,100 @@
+
+import * as cmdl from "./common.js"
+
+/** @type {HTMLElement} */
+let calendarBox = document.getElementById("cl.task-entry-box").cloneNode(true);
+/** @type {HTMLElement} */
+let calendarGrid = document.getElementById("cl.grid-view");
+/** @type {HTMLElement} */
+let emptyRow = document.getElementById("cl.grid-view.row").cloneNode(false);
+
+
+calendarBox.removeAttribute("id");
+/** @type {HTMLElement} */
+let emptyCalendarBox = calendarBox.cloneNode(true); {
+    let dayNumberText = emptyCalendarBox.querySelector(".day_number")
+    emptyCalendarBox.removeChild(dayNumberText)
+    emptyCalendarBox.innerHTML = ""
+    emptyCalendarBox.appendChild(dayNumberText)
+}
+
+let CalenderView = {
+    updateToMonthAndYear : function(year, month) {
+        calendarGrid.innerHTML = ""
+        let day = 1;
+
+        let firstDayOfMonth = new Date(year, month)
+        let lastDayOfMonth = (new Date(firstDayOfMonth)); {
+            lastDayOfMonth.setMonth(lastDayOfMonth.getMonth() + 1)
+            lastDayOfMonth.setDate(lastDayOfMonth.getDate() - 1)
+        }
+
+        let totalDaysInMonth = lastDayOfMonth.getDate() - firstDayOfMonth.getDate() + 1
+
+        let firstDayOfFirstWeek = new Date(firstDayOfMonth)
+        let lastDayOfLastWeek = new Date(lastDayOfMonth);
+        
+        firstDayOfFirstWeek.setDate(firstDayOfFirstWeek.getDate() - firstDayOfFirstWeek.getDay())
+        lastDayOfLastWeek.setDate(lastDayOfLastWeek.getDate() + 6 - lastDayOfLastWeek.getDay())
+
+        let firstDaysBeforeScope = (firstDayOfMonth.getTime() - firstDayOfFirstWeek.getTime()) / cmdl.milli.day
+        let totalDaysInScope = (
+            lastDayOfLastWeek.getTime()
+            - firstDayOfFirstWeek.getTime())
+            / cmdl.milli.day + 1
+        let totalWeeksInScope = totalDaysInScope / 7
+
+        let currentInstant = new Date(firstDayOfFirstWeek)
+
+        for (let currentWeek = 0; currentWeek < totalWeeksInScope; currentWeek++) {
+            /** @type {HTMLElement} */
+            let currentRow = emptyRow.cloneNode(false)
+
+            for (let currentDay = 0; currentDay < 7; currentDay++) {
+                switch(true) {
+                    case (firstDaysBeforeScope <= 0 && totalDaysInMonth > 0): {
+                        let placeTasks = Math.random() > 0.5;
+                        let element;
+
+                        if (placeTasks) {element = calendarBox.cloneNode(true);}
+                        else {element = emptyCalendarBox.cloneNode(true);}
+
+                        element.querySelector(".day_number").textContent = currentInstant.getDate();
+                        currentRow.appendChild(element)
+                        totalDaysInMonth--
+                        break
+                    }
+                    case (firstDaysBeforeScope > 0): firstDaysBeforeScope--
+                    case (totalDaysInMonth <= 0): {
+                        /** @type {HTMLElement} */
+                        let element = emptyCalendarBox.cloneNode(true)
+                        element.querySelector(".day_number").textContent = currentInstant.getDate();
+                        element.classList.add(["disabled"])
+                        currentRow.appendChild(element)
+                        break
+                    }
+                }
+
+                currentInstant.setTime(currentInstant.getTime() + cmdl.milli.day)
+            }
+
+            calendarGrid.appendChild(currentRow)
+        }
+    }
+}
+
+let clock = (new cmdl.LiveTimeDisplay(
+    new Intl.DateTimeFormat(
+        undefined,
+        {
+            "dateStyle": "medium"
+        }
+    )
+)).addElement(document.getElementById("cl.date"))
+    .setIntervalLength(60000)
+    .start()
+
+clock.handler()
+
+let today = new Date(clock.getNow())
+CalenderView.updateToMonthAndYear(today.getFullYear(), today.getMonth())
